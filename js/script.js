2d6d01a8e36b639875867da746511164aa3dc30d
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-26 14:40:35 +0100

    feat: add html h1 text

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // Descrizione:
 
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: 'Vue Hello 1st exercise'
+    };
+  }
+}).mount('#app');
 
 // Bonus:
 
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
-// const { createApp } = Vue;
-
-// createApp({
-//   data() {
-//     return {
-//       message: 'text here'
-//     };
-//   }
-// }).mount('#app');
 
